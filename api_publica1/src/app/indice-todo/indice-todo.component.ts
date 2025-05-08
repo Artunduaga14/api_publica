09b7c32d71b25664cc0a0dae7f22472ceb7cf560
Artunduaga14 <artunduagajuan21@gmail.com>
2025-05-08 07:08:41 -0500

    suvo nuevo registro Todo

--- api_publica1/src/app/indice-todo/indice-todo.component.ts
+++ api_publica1/src/app/indice-todo/indice-todo.component.ts
@@ -1,27 +1,28 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-indice-todo',
-//   imports: [],
-//   templateUrl: './indice-todo.component.html',
-//   styleUrl: './indice-todo.component.css'
-// })
-// export class IndiceTodoComponent {
-
-// }
 
 import { Component, inject, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todos.model';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-indice-todo',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './indice-todo.component.html',
   styleUrl: './indice-todo.component.css'
 })
 export class IndiceTodoComponent implements OnInit {
   todos: Todo[] = [];
+  columnas: string[] = ['id', 'title', 'completed', 'acciones'];
 
   todoService = inject(TodoService)
 
@@ -36,4 +37,12 @@ export class IndiceTodoComponent implements OnInit {
       }
    });
   }
+
+   editar(todo: Todo) {
+      alert(`Simular editar usuario: ${todo.title}`);
+    }
+  
+    eliminar(todo: Todo) {
+      alert(`Simular eliminar usuario: ${todo.title}`);
+    }
 }
--- api_publica1/src/app/indice-user/indice-user.component.ts
+++ api_publica1/src/app/indice-user/indice-user.component.ts
@@ -1,27 +1,49 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { UserService } from '../services/user.service';
 import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-indice-user',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './indice-user.component.html',
-  styleUrl: './indice-user.component.css'
+  styleUrls: ['./indice-user.component.css']
 })
 export class IndiceUserComponent implements OnInit {
   users: User[] = [];
+  columnas: string[] = ['id', 'name', 'email', 'username', 'acciones'];
 
-  userService = inject(UserService)
+  userService = inject(UserService);
 
   ngOnInit(): void {
     this.userService.getAll().subscribe({
-      next: (sdsassa) => {
-        this.users = sdsassa;
+      next: (usuarios) => {
+        this.users = usuarios;
         console.log('Usuarios recibidos:', this.users);
       },
       error: (err) => {
         console.error('Error al obtener usuarios:', err);
       }
-   });
+    });
+  }
+
+  editar(user: User) {
+    alert(`Simular editar usuario: ${user.name}`);
+  }
+
+  eliminar(user: User) {
+    alert(`Simular eliminar usuario: ${user.name}`);
   }
 }
